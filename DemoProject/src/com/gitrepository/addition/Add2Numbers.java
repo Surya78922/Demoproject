c3640de8a797f3c73b3e9204f0f2c0a4d613b169
Admin <Admin@DESKTOP-0H54U56>
2022-02-03 13:04:54 +0530

    first comment

---
+++ DemoProject/src/com/gitrepository/addition/Add2Numbers.java
@@ -0,0 +1,14 @@
+package com.gitrepository.addition;
+
+public class Add2Numbers {
+	
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+		
+	}
+
+}
